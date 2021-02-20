0345e04b9258e7e7a8a34cb2c9d3a15b3eda279e
knuddy <dean.j.knudson@gmail.com>
2021-02-21 12:27:49 +1300

    fixed sql for updating ban list

--- database/banned_from/update.py
+++ database/banned_from/update.py
@@ -2,6 +2,6 @@ def add_to_banned_list(db, subreddit):
     insert_sql = f"""
         UPDATE banned_from
         SET usernames = array_append(subreddits, '{subreddit}')
-        WHERE id=1'
+        WHERE id=1
     """
     db.execute(insert_sql)
